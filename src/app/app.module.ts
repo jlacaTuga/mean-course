3bc5d713702eb3f039ec7a5f5b53d99f00044a54
jlacaTuga <jlaca@live.com.pt>
2022-12-30 17:42:09 -0300

    Lista de Posts expansivel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,19 @@ import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {HeaderComponent} from "../header/header.component";
+import {PostListComponent} from "./posts/post-list/post-list.component";
+import {MatExpansionModule} from "@angular/material/expansion";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+
+
   ],
   imports: [
     BrowserModule,
@@ -19,7 +27,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
